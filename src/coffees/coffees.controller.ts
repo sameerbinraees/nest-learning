fd211e7f61d9a8c616a8afd99ed48202aad8e8e9
Muhammad Sameer Bin Raees <44906406+sameerbinraees@users.noreply.github.com>
2022-12-25 18:49:11 +0500

    first module

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() queryParams) {
+    // const { limit, offset } = queryParams;
+    // return `All coffees Limit ${limit}, Offset ${offset}`;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // return `Return the value for a specific id: ${id}`;
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    // return body;
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    // return `This end point updates ${id}`;
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    // return `This end point deletes ${id}`;
+    return this.coffeesService.remove(id);
+  }
+}
